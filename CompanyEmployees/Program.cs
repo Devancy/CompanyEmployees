903f05f64ce19e5c01ba75a6d3a40cf7f0da9d64
Diep Pham <pdiep@tibco.com>
2024-02-27 14:19:06 +0700

    add middlewares services (log, staticfiles, router, cors)

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,14 +1,35 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    // The HTTP Strict-Transport-Security response header informs browsers that the site should only be accessed using HTTPS,
+    // and that any future attempts to access it using HTTP should automatically be converted to HTTPS.
+    // Note: This is more secure than simply configuring a HTTP to HTTPS (301)
+    app.UseHsts();
+}
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
