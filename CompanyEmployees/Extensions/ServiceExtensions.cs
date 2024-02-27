b725ca76a0f968463a8bafca27953f9a1cb48b9b
Diep Pham <pdiep@tibco.com>
2024-02-27 14:54:05 +0700

    add nlog as logger service

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace CompanyEmployees.Extensions;
+﻿using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions;
 
 public static class ServiceExtensions
 {
@@ -16,4 +19,7 @@ public static class ServiceExtensions
         {
 
         });
+
+    public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerManager, LoggerManager>();
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,12 +1,18 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 
 // Add services to the container.
 
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
